6128df272346abd6c8135be28180f3f6d90a125e
RoiSukenik <roisukenik@gmail.com>
2020-12-11 19:09:00 +0200

    Prime num list calculator finished - not yet tested

--- Ex_3/Factori/prime_number_calculator.c
+++ Ex_3/Factori/prime_number_calculator.c
@@ -1,6 +1,61 @@
 #include "prime_number_calculator.h"
 
-int* initialize_prime_num_array()
+node* initialize_prime_num_list()
 {
-	
+	node* p_prime_num_arr = NULL;
+	p_prime_num_arr = (node*)malloc(sizeof(node));
+	if (NULL == p_prime_num_arr)
+	{
+		printf_s("Failed to allocate memory");
+		exit(STATUS_CODE_FAILURE);
+	}
+	return p_prime_num_arr;
+}
+
+node* add_prime(node* prime_num_arr , int num)
+{
+	int value = 0;
+	node* new_node = NULL;
+	while (num > 0)
+	{
+		new_node = (node*)malloc(sizeof(node));
+		if (NULL == new_node) {
+			printf_s("Failed to allocate memory");
+			exit(STATUS_CODE_FAILURE);
+		}
+		new_node->data = value;
+		new_node->next = prime_num_arr;
+	}
+	return new_node;
+}
+
+node* divid_number_add_2_list(int num)
+{
+	node* p_prime_list = initialize_prime_num_list();
+	while (num % 2 > 0)
+	{
+		p_prime_list = add_prime(p_prime_list ,2);
+		num = num / 2;
+	}
+	for (int i = 3; i <= sqrt(num); i = i + 2)
+	{
+		while (num % i == 0)
+		{
+			p_prime_list = add_prime(p_prime_list , i);
+			num = num / i;
+		}
+	}
+	if (num > 2) p_prime_list = add_prime(p_prime_list , num);
+
+	return p_prime_list;
+}
+void free_list(node* p_prime_list)
+{
+	node* temp;
+	while (p_prime_list != NULL)
+	{
+		temp = p_prime_list;
+		p_prime_list = p_prime_list->next;
+		free(temp);
+	}
 }
--- Ex_3/Factori/prime_number_calculator.h
+++ Ex_3/Factori/prime_number_calculator.h
@@ -1,27 +1,47 @@
 #ifndef PRIME_NUMBER_CALCULATOR_DOT_H
 #define PRIME_NUMBER_CALCULATOR_DOT_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define STATUS_CODE_FAILURE 1
+#define STATUS_CODE_SUCCESS 0
+
+
+typedef struct node {
+	int data;
+	node* next;
+}node;
+
 /*
 * Description - Initialize prime number array, heap based
 * Parameters - None
 * Returns - Pointer to a array struct
 */
-int* initialize_prime_num_array();
+int* initialize_prime_num_list();
 
 /*
-* Description - Fill prime number array with the prime numbers
-* Parameters - Pointer to number array
-* Returns - Pointer to the updated number array
+* Description - realloc in prime num arr, add prime number, update pointer
+* Parameters -  int to add
+* Returns - Pointer to the updated number list
 */
-int* divid_number(int* number);
+node* add_prime(node* prime_num_arr , int num);
+
+/*
+* Description - Fill prime number list with the prime numbers
+* Parameters -  int to divid
+* Returns - Pointer to the updated number list
+*/
+node* divid_number_add_2_list(int num);
 
 
 /*
-* Description - Free prime number array
-* Parameters - Pointer to number array
-* Returns - set array pointer to null
+* Description - Free prime number list
+* Parameters - Pointer to number list
+* Returns - set list pointer to null
 */
-void free_array(int* number);
+void free_list(node* p_prime_list);
 
 
 
