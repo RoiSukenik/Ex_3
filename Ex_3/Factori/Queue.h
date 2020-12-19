9535eb3604715e7c5e0ea50efb14cbe9d6505cb9
Eldor Fadida <eldorfadida@mail.tau.ac.il>
2020-12-19 23:28:15 +0200

    fixed queue h/c mismatch

--- Ex_3/Factori/Queue.h
+++ Ex_3/Factori/Queue.h
@@ -35,21 +35,21 @@ Queue *InitializeQueue(void);
 * Parameters - Pointer to a Queue type object
 * Returns - int which is at the top the given queue
 */
-int Top(Queue* queue);
+int Top(Queue* que);
 
 /*
 * Description - Pop the first object in a queue
 * Parameters - Pointer to a Queue type object
 * Returns - int of the poped item, poped node is freed , next elemnt is now top of queue
 */
-int Pop(Queue* queue);
+int Pop(Queue* que);
 
 /*
 * Description - Push an element to the end of the queue
 * Parameters - Pointer to a Queue type object and an integer data object
 * Returns - void, nothing
 */
-void Push(Queue* queue, int value);
+int Push(Queue* que, int value);
 
 
 /*
@@ -57,14 +57,14 @@ void Push(Queue* queue, int value);
 * Parameters - Pointer to a Queue type object
 * Returns - True if queue is empty | False if queue is not empty
 */
-bool Empty(Queue* queue);
+bool Empty(Queue* que);
 
 /*
 * Description - Destroys Queue
 * Parameters - Pointer to a Queue type object
 * Returns - Free all queue memory resources, set pointer to NULL
 */
-Queue *DestroyQueue(Queue* queue);
+Queue *DestroyQueue(Queue* que);
 
 #endif // !QUEUE_DOT_H
 
