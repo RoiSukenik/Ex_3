fd74894bcd6a4df515fd47ddbacbf0b9fb3dd737
Eldor Fadida <eldorfadida@mail.tau.ac.il>
2020-12-19 23:07:33 +0200

    Unchanged

---
+++ Ex_3/Factori/includes.h
@@ -0,0 +1,12 @@
+#ifndef INCLUDES_DOT_H
+#define INCLUDES_DOT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <stdbool.h>
+#include <windows.h>
+#include <crtdbg.h>
+#include <fileapi.h>
+#endif // !INCLUDES_DOT_H
